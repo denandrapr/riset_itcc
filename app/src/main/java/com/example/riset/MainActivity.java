ae856215237b9f2fbf4321198fadde43f607ac86
archinemis <denandra1628@gmail.com>
2019-09-13 23:09:59 +0700

    commit pertama

---
+++ app/src/main/java/com/example/riset/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.riset;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.riset.Donasi.DonasiFragment;
+import com.example.riset.Home.HomeFragment;
+import com.example.riset.Profile.ProfileFragment;
+import com.example.riset.Riwayat.RiwayatFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.bottom_nv)
+    BottomNavigationView btm_nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        loadFragment(new HomeFragment());
+        btm_nv.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_root, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.riwayat:
+                fragment = new RiwayatFragment();
+                break;
+            case R.id.donasi:
+                fragment = new DonasiFragment();
+                break;
+            case R.id.saya:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+}
