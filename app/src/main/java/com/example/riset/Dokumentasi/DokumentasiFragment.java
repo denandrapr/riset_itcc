b6113645d9497674cd7b274c7a75ceb50cf681e1
archinemis <denandra1628@gmail.com>
2019-10-03 17:51:03 +0700

    progress 5%

--- app/src/main/java/com/example/riset/Dokumentasi/DokumentasiFragment.java
+++ app/src/main/java/com/example/riset/Dokumentasi/DokumentasiFragment.java
@@ -8,15 +8,50 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.riset.Dokumentasi.Adapter.DokumentasiAdapter;
+import com.example.riset.Home.Adapter.TerdekatKamuAdapter;
 import com.example.riset.R;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DokumentasiFragment extends Fragment {
+
+    @BindView(R.id.recycler)
+    RecyclerView recyclerView;
+
+    DokumentasiAdapter dokumentasiAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dokumentasi_fragment, container, false);
+        ButterKnife.bind(this, view);
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Bantu Bella agar dapat bersekolah \n" +
+                "lagi untuk meraih mimpinya");
+        animalNames.add("Bantu Bella agar dapat bersekolah \n" +
+                "lagi untuk meraih mimpinya");
+        animalNames.add("Bantu Bella agar dapat bersekolah \n" +
+                "lagi untuk meraih mimpinya");
+        animalNames.add("Bantu Bella agar dapat bersekolah \n" +
+                "lagi untuk meraih mimpinya");
+        animalNames.add("Bantu Bella agar dapat bersekolah \n" +
+                "lagi untuk meraih mimpinya");
+
+        dokumentasiAdapter = new DokumentasiAdapter(getActivity(), animalNames);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(dokumentasiAdapter);
+
         return view;
     }
 
