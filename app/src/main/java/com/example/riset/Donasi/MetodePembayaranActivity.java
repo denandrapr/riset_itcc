627e9723a99412b724aa62ed1663c2b00f0a7a1c
archinemis <denandra1628@gmail.com>
2019-10-14 22:12:07 +0700

    berdonasi

--- app/src/main/java/com/example/riset/Donasi/MetodePembayaranActivity.java
+++ app/src/main/java/com/example/riset/Donasi/MetodePembayaranActivity.java
@@ -1,10 +1,18 @@
 package com.example.riset.Donasi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.riset.R;
 
@@ -12,15 +20,21 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MetodePembayaranActivity extends AppCompatActivity {
 
     ArrayList<String> list;
     ArrayAdapter adapter;
-    String[] bank = {"BCA", "Mandiri", "BRI", "BNI", "BNI Syariah", "Danamon", "CIMB Niaga"};
+    String metodeDiPilih;
+String[] bank = {"BCA", "Mandiri", "BRI", "Alfamart"};
 
     @BindView(R.id.listView)
     ListView mListView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.pilihMetode)
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +42,43 @@ public class MetodePembayaranActivity extends AppCompatActivity {
         setContentView(R.layout.activity_metode_pembayaran);
         ButterKnife.bind(this);
 
+        btn.setVisibility(View.GONE);
+
+        SharedPreferences mSettings = MetodePembayaranActivity.this.getSharedPreferences("Settings", Context.MODE_PRIVATE);
         list = new ArrayList<>();
         for (int i = 0; i < bank.length; i++){
             list.add(bank[i]);
         }
         mListView.setChoiceMode(mListView.CHOICE_MODE_SINGLE);
-//        adapter = new ArrayAdapter(MetodePembayaranActivity.this, R.)
+        adapter = new ArrayAdapter(MetodePembayaranActivity.this, android.R.layout.simple_list_item_single_choice, list);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                metodeDiPilih = bank[i];
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putString("pilihMetode", bank[i]);
+                editor.apply();
+                btn.setText("Pilih metode "+bank[i]);
+                btn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @OnClick(R.id.pilihMetode)
+    void pilihMetode(){
+        onBackPressed();
     }
 }
