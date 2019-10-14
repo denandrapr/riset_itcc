ffe05b602b7b8f0feae60b98c3f9041516b604f5
archinemis <denandra1628@gmail.com>
2019-10-14 12:13:24 +0700

    commit di perpus

--- app/src/main/java/com/example/riset/Donasi/BerdonasiStep1Activity.java
+++ app/src/main/java/com/example/riset/Donasi/BerdonasiStep1Activity.java
@@ -2,21 +2,61 @@ package com.example.riset.Donasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import com.example.riset.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class BerdonasiStep1Activity extends AppCompatActivity {
+
+    private Dialog customDialog;
+
+    @BindView(R.id.pilihMetodeBayar)
+    Button pilihMetodeBayar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berdonasi_step1);
         ButterKnife.bind(this);
+
+        initViews();
+    }
+
+    private void initViews(){
+        initCustomDialog();
+        initViewsComponent();
+    }
+
+    private void initCustomDialog(){
+        customDialog = new Dialog(BerdonasiStep1Activity.this);
+        customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        customDialog.setContentView(R.layout.dialog_custom);
+        customDialog.setCancelable(true);
+
+        pilihMetodeBayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog.dismiss();
+            }
+        });
+    }
+
+    private void initViewsComponent(){
+        pilihMetodeBayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog.show();
+            }
+        });
     }
 
     @OnClick(R.id.lanjutkan_pembayaran)
--- app/src/main/java/com/example/riset/Donasi/MetodePembayaranActivity.java
+++ app/src/main/java/com/example/riset/Donasi/MetodePembayaranActivity.java
@@ -3,14 +3,36 @@ package com.example.riset.Donasi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.riset.R;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MetodePembayaranActivity extends AppCompatActivity {
+
+    ArrayList<String> list;
+    ArrayAdapter adapter;
+    String[] bank = {"BCA", "Mandiri", "BRI", "BNI", "BNI Syariah", "Danamon", "CIMB Niaga"};
+
+    @BindView(R.id.listView)
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metode_pembayaran);
+        ButterKnife.bind(this);
+
+        list = new ArrayList<>();
+        for (int i = 0; i < bank.length; i++){
+            list.add(bank[i]);
+        }
+        mListView.setChoiceMode(mListView.CHOICE_MODE_SINGLE);
+//        adapter = new ArrayAdapter(MetodePembayaranActivity.this, R.)
     }
 }
