2341d1231b546df13e71bcdd1bec174278879360
archinemis <denandra1628@gmail.com>
2019-10-22 12:56:11 +0700

    galang dana fix 95%

--- app/src/main/java/com/example/riset/Berdonasi/GalangDanaStep4Activity.java
+++ app/src/main/java/com/example/riset/Berdonasi/GalangDanaStep4Activity.java
@@ -2,11 +2,14 @@ package com.example.riset.Berdonasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.riset.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GalangDanaStep4Activity extends AppCompatActivity {
 //@BindView(R.id.)
@@ -14,5 +17,13 @@ public class GalangDanaStep4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galang_dana_step4);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_selesai)
+    void btn_selesai(){
+        Intent i = new Intent(GalangDanaStep4Activity.this, BuatKegiatanActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 }
